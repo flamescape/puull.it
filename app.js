fa392a02a2f7738bf480440dc9295164eeae0ede
Mikuso <g_github@flamescape.com>
2014-04-09 20:27:45 +0100

    Getting Angular :triangular_ruler:

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require('express')
   , request = require('request')
   ;
 
+  
 app.get('/dl/:id', function(req, res, next){
     request.get('http://puu.sh/'+req.params.id).pipe(res);
 });
+
+app.use(express.static('public'));
 
 app.listen(80);
---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+
+angular.module('app', [])
+
+    .controller('ListCtrl', function($scope){
+    
+        $scope.puushes = [{img: '6Z'}, {img: '70'}];
+    
+    })
+    
+;
