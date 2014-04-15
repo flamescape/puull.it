5b8ea4f56acc009fbd866ac49a4eebb4bc0e9041
Gareth Hughes <g_github@flamescape.com>
2014-04-15 20:54:20 +0100

    Add ability to find work

--- js/auto-puuller.js
+++ js/auto-puuller.js
@@ -1,6 +1,7 @@
 var _ = require('underscore')
   , fs = require('fs')
   , async = require('async')
+  , Promise = require('bluebird')
   ;
   
 module.exports = function(db, puush){
@@ -21,12 +22,27 @@ module.exports = function(db, puush){
             .catch(function(err){ console.log('Caught err:', err); cb(); });
     }, 5);
     
+    var lastRecordedPid = function(){
+        return new Promise(function(resolve, reject){
+            db.find({}).sort({pid:-1}).limit(1).exec(function(err, rows){
+                if (err) return reject(err);
+                if (!rows.length) return resolve(null);
+                resolve(rows[0].pid);
+            });
+        });
+    };
+    
     var findWork = function(){
         console.log('Work queue empty. Finding work to do.');
-        // TODO: find min/max pids, push to the queue
-        puush.getEnd().then(function(pid){
-            for (var i = 0; i < 100; i++) {
-                workQueue.push(pid - 500 - i);
+
+        Promise.all([
+            lastRecordedPid(),
+            puush.getEnd()
+        ]).spread(function(min, max){
+            max -= 500; // we don't want the very latest, as many won't have actually finished uploading yet
+            if (!min) min = max - 500; // if we have an empty datastore, we'll need to start somewhere
+            for (var i = min; i < max; i++) {
+                workQueue.push(i);
             }
         });
     };
